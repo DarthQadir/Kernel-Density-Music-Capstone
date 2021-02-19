6967afacd3afc9f58be42449f2d4bf6dcf5904ad
darthqadir <darthqadir@live.com>
2021-02-19 21:43:03 +0500

    added classes for separation of concerns and fixed bugs

--- gui.py
+++ gui.py
@@ -1,40 +1,68 @@
 import tkinter as tk
 import tkinter.font as tkFont
-import density_estimation
-import tab_preprocessing
+from density_estimation import density_estimator
+from tab_preprocessing import tab_preprocessor
 import music_theory
 
 from tkinter import ttk
 
 
-#Create the Tkinter window and subwindows that will be used
+
+
+
+"""
+Create the Tkinter window and subwindows that will be used
+"""
+
+#Main window
 window = tk.Tk()
 window.title("Input Tablature")
 
+
+#Sub windows
 popup_moretab = tk.Toplevel()
 popup_mode = tk.Toplevel()
 popup_print = tk.Toplevel()
 
 
+#Withdraw subwindows so we show them when required
 popup_moretab.withdraw()
 popup_mode.withdraw()
 popup_print.withdraw()
 
+
+#Give the windows text titles as explanation
+popup_moretab.title('Input more tabs?')
+popup_mode.title('Choose key and mode of song')
 popup_print.title('Outputted Guitar Tabs')
 
+#Create variables to store key and mode of song
+textvar_key = tk.StringVar()
+textvar_mode = tk.StringVar()
 
 
+"""
+Code that makes a guide on how to use the program
+"""
 
-
-#Make explanation
+#Make guide on how to use program
 label_explanation = tk.Label(master=window, height=12, width=90,text=
 "\n\n\n 1- Copy paste your tablature in the box below and press enter \n \n 2- If you need to input more tablatures, click yes in the next prompt, or if you're done, click no' \n \n 3- Input which key and mode you want your output to be in. \n \n 4- Get new tablature!")
 label_explanation.grid(row=0,column=0)
 
-#Make title
+
+#Make title for the guide
 run_font = tkFont.Font(family="Lucida Grande", size=17)
 run_label = tk.Label(master=window,text='How to run the program',font=run_font)
 run_label.place(x=283,y=16)
+
+
+
+
+"""
+Code that sets up our UI for inputting guitar tablature
+"""
+
 
 #Make input box for guitar tabs
 input_box = tk.Text(master=window,height=8, width=100,borderwidth=3)
@@ -42,26 +70,31 @@ input_box.grid(row=1,column=0,pady=10)
 input_box.insert(tk.END,'Enter guitar tab here......') 
 
 
-#Code that clears the input guitar tab text
+#Code that clears the input guitar tab text; mimicking softwares which clear text when you click on a box
 def clear_textbox(event):
     if input_box.get('1.0',tk.END) == 'Enter guitar tab here......\n':
         input_box.delete('1.0', tk.END)
 input_box.bind("<Button-1>", clear_textbox)
 
 
-#Add buttons
+#Add button that clears tablature
 clear_button = tk.Button(master=window, text='Clear input')
 clear_button.grid(row=2,column=0,pady=5)
 
 
-#Code that clears all input
+#Code that clears all input once the button defined above is clicked
 def clear_tab(event):
     input_box.delete('1.0',tk.END)
     
 clear_button.bind("<Button-1>",clear_tab)
-
+  
+    
+    
     
 def clear_next_tab(event):
+    """
+    Resets our UI when we need to input more tabs
+    """
     window.deiconify()
     input_box.delete('1.0',tk.END)
     input_box.insert(tk.END,'Enter guitar tab here......')
@@ -71,6 +104,10 @@ def clear_next_tab(event):
 
 
 def ask_tab(event):
+    """
+    Code that initializes a new UI to ask for more tablature input
+    Also writes our inputted tabs to a text file
+    """
     
     #Write tabs to text file
     with open('tabs.txt', 'a') as tab_text:
@@ -78,7 +115,6 @@ def ask_tab(event):
         
     popup_moretab.deiconify()
     window.withdraw()
-    popup_moretab.title('Input more tabs?')
     input_text = tk.Label(master=popup_moretab,text='Do you want to input more tabs?',width=30)
     input_text.grid(row=0,column=0,columnspan=2)
     
@@ -94,10 +130,15 @@ input_box.bind("<Return>",ask_tab)
 
 
 def mode_choose(event):
+    """
+    Code that initializes a new UI to ask for the key/mode of a song
+    """
+    
+    
     popup_mode.deiconify()
     popup_moretab.withdraw()
     window.withdraw()
-    popup_mode.title('Choose key and mode of song')
+    
     
     keys_label=tk.Label(master=popup_mode,text='Choose Key')
     keys_label.grid(row=0,column=0)
@@ -105,9 +146,8 @@ def mode_choose(event):
     mode_label=tk.Label(master=popup_mode,text='Choose Mode')
     mode_label.grid(row=0,column=1)
     
-    global textvar_key, textvar_mode
-    textvar_key = tk.StringVar()
-    textvar_mode = tk.StringVar()
+    #global textvar_key, textvar_mode
+
     keys = ttk.Combobox(master=popup_mode, textvariable = textvar_key)
     modes = ttk.Combobox(master=popup_mode, textvariable=textvar_mode)
     
@@ -126,19 +166,25 @@ def mode_choose(event):
     
     
 def machine_learning(event):
+    """
+    Code that carries out kernel density estimation and outputs our generated notes
+    """
     popup_mode.withdraw()
     window.withdraw()
     with open('tabs.txt','r') as read_tabs:
         tabs = read_tabs.readlines()
         tabs = [''.join(tabs[i:i+6]).replace('\n',' ') for i in range(0,len(tabs),6)]
     
-    tab_lengths = tab_preprocessing.length_check(tabs)
-    tab_list = tab_preprocessing.tab_to_list(tabs,tab_lengths)
-    final_notes = tab_preprocessing.vertical_slice(tab_list[0],tab_list[1],tab_list[2],tab_list[3],tab_list[4],tab_list[5],tab_list[6])
-    final_notes = tab_preprocessing.remove_wrong_notes(final_notes)
+    tab_processor = tab_preprocessor(tabs)
+    tab_lengths = tab_processor.length_check()
+    tab_list = tab_processor.tab_to_list(tab_lengths)
+    final_notes = tab_processor.vertical_slice(tab_list[6])
+    final_notes = tab_processor.remove_wrong_notes(final_notes)
     
-    model = density_estimation.density_estimate(final_notes)
-    samples = density_estimation.generate_samples(model)
+
+    model = density_estimator()
+    model.density_estimate(final_notes)
+    samples = model.generate_samples()
     new_notes = music_theory.mode_notes(textvar_mode.get(),textvar_key.get())
     processed_samples = music_theory.filter_notes(music_theory.e_notes,music_theory.B_notes,
                                             music_theory.G_notes,music_theory.D_notes,
--- tab_preprocessing.py
+++ tab_preprocessing.py
@@ -1,256 +1,270 @@
 
 
-def length_check(tabs):
-    """   
-    Function that returns the length of each tablature that is input into the program.
+
+
+
+class tab_preprocessor():
+
+    def __init__(self,tabs):
     
-    Checks the length by measuring distance between the '|' symbol in tabs
-    """
+        #Guitar tab
+        self.tabs = tabs
+        
 
-    tab_lengths = []
-    for tab in tabs:
-      start_check = False
-      for i in range(len(tab)):
+        #Arrays to hold each string's notes
+        self.string_e = []
+        self.string_B = []
+        self.string_G = []
+        self.string_D = []
+        self.string_A = []
+        self.string_E = []
+        
+        #Checks for optimization purposes
+        self.check_e = False
+        self.check_B = False
+        self.check_G = False
+        self.check_D = False
+        self.check_A = False
+        self.check_E = False
+        
+        self.final_notes = []
+        self.notes = []
+        
 
-        if start_check == False:
-          if tab[i] == '|':
-            start = i
-            start_check = True
+    def length_check(self):
+        """   
+        Function that returns the length of each tablature that is input into the program.
+        
+        Checks the length by measuring distance between the '|' symbol in tabs
+        """
 
-        else:
-          if tab[i] == '|':
-            end = i
-            break
-      tab_lengths.append(end - start - 1)
+        tab_lengths = []
+        for tab in self.tabs:
+          start_check = False
+          for i in range(len(tab)):
+
+            if start_check == False:
+              if tab[i] == '|':
+                start = i
+                start_check = True
+
+            else:
+              if tab[i] == '|':
+                end = i
+                break
+          tab_lengths.append(end - start - 1)
+          
+        return tab_lengths
+        
       
-    return tab_lengths
-    
-  
 
 
-def add_dash(string,longest_len):
-    """
-    Helper function for tab_to_list that adds dashes to strings to make each string the same length
-    """
-    while len(string) < longest_len:
-      string += '-'  
+    def add_dash(self,string,longest_len):
+        """
+        Helper function for tab_to_list that adds dashes to strings to make each string the same length
+        """
+        while len(string) < longest_len:
+          string += '-'  
 
-def get_notes(tab,i,string_type,tab_lengths,count):
-    """
-    Helper function for tab_to_list that appends the right notes from the string list to another list
-    """
-    if eval('check_'+string_type) == False:
-        if tab[i] == string_type:
-          eval('string_'+string_type).append(tab[i+2:i+2+tab_lengths[count]])
-          temp = eval('check_'+string_type)
-          temp = True
+    def get_notes(self,tab,i,string_type,tab_lengths,count):
+        """
+        Helper function for tab_to_list that appends the right notes from the string list to another list
+        """
+        if eval('self.check_'+string_type) == False:
+            if tab[i] == string_type:
+              eval('self.string_'+string_type).append(tab[i+2:i+2+tab_lengths[count]])
+              temp = eval('self.check_'+string_type)
+              temp = True
 
 
 
-def tab_to_list(tabs,tab_lengths):
-    """
-    Function that adds each string's notes from each tab to a list.
-    
-    Returns each string's notes in a list
-    """
-    #Arrays to hold each string's notes
-    global string_e,string_B,string_G,string_D,string_A,string_E
-    string_e = []
-    string_B = []
-    string_G = []
-    string_D = []
-    string_A = []
-    string_E = []
-
-
-    count = 0
+    def tab_to_list(self,tab_lengths):
+        """
+        Function that adds each string's notes from each tab to a list.
+        
+        Returns each string's notes in a list
+        """
 
 
 
-    #Put the notes for each string in the aforementioned lists
-    for tab in tabs:
-
-      #Checks for optimization purposes
-      global check_e,check_B,check_G,check_D,check_A,check_E
-      check_e = False
-      check_B = False
-      check_G = False
-      check_D = False
-      check_A = False
-      check_E = False
-      
-      for i in range(len(tab)):   
-        get_notes(tab,i,'e',tab_lengths,count)
-        get_notes(tab,i,'B',tab_lengths,count)
-        get_notes(tab,i,'G',tab_lengths,count)
-        get_notes(tab,i,'D',tab_lengths,count)
-        get_notes(tab,i,'A',tab_lengths,count)
-        get_notes(tab,i,'E',tab_lengths,count)
-
-      count+=1
-
-    
-    #Now we have the tabs in a prettier format
-
-    string_e = ''.join(string_e)
-    string_B = ''.join(string_B)
-    string_G = ''.join(string_G)
-    string_D = ''.join(string_D)
-    string_A = ''.join(string_A)
-    string_E = ''.join(string_E)
+        count = 0
 
 
 
-    #Make each string of a consistent length
-    longest_len = max(len(string_e),len(string_B),len(string_G),len(string_D),len(string_A),len(string_E))
+        #Put the notes for each string in the aforementioned lists
+        for tab in self.tabs:
+          
+          for i in range(len(tab)):   
+            self.get_notes(tab,i,'e',tab_lengths,count)
+            self.get_notes(tab,i,'B',tab_lengths,count)
+            self.get_notes(tab,i,'G',tab_lengths,count)
+            self.get_notes(tab,i,'D',tab_lengths,count)
+            self.get_notes(tab,i,'A',tab_lengths,count)
+            self.get_notes(tab,i,'E',tab_lengths,count)
+
+          count+=1
+
+        
+        #Now we have the tabs in a prettier format
+
+        self.string_e = ''.join(self.string_e)
+        self.string_B = ''.join(self.string_B)
+        self.string_G = ''.join(self.string_G)
+        self.string_D = ''.join(self.string_D)
+        self.string_A = ''.join(self.string_A)
+        self.string_E = ''.join(self.string_E)
 
 
 
-    add_dash(string_e,longest_len)
-    add_dash(string_B,longest_len)
-    add_dash(string_G,longest_len)
-    add_dash(string_D,longest_len)
-    add_dash(string_A,longest_len)
-    add_dash(string_E,longest_len)
-      
-    return (string_e,string_B,string_G,string_D,string_A,string_E,longest_len)
-    
- 
- 
- 
- 
- 
-def twelve_note(i):
-    """
-    Helper function for vertical_slice. It appends two vertical slices of our tab
-    """
-    notes.append(string_e[i])
-    notes.append(string_B[i])
-    notes.append(string_G[i])
-    notes.append(string_D[i])
-    notes.append(string_A[i])
-    notes.append(string_E[i])
-    notes.append(string_e[i+1])
-    notes.append(string_B[i+1])
-    notes.append(string_G[i+1])
-    notes.append(string_D[i+1])
-    notes.append(string_A[i+1])
-    notes.append(string_E[i+1])
-    final_notes.append(notes)
-
-def six_note(i):
-    """
-    Helper function for vertical_slice. It appends one vertical slice of our tab
-    """
-    notes.append(string_e[i])
-    notes.append(string_B[i])
-    notes.append(string_G[i])
-    notes.append(string_D[i])
-    notes.append(string_A[i])
-    notes.append(string_E[i])
-    final_notes.append(notes)
+        #Make each string of a consistent length
+        longest_len = max(len(self.string_e),len(self.string_B),len(self.string_G),len(self.string_D),len(self.string_A),len(self.string_E))
 
 
-def note_check(string,i,longest_len):
-    """
-    Helper function for vertical_slice. It checks whether we have a 2 digit or 1 digit note
-    """
-    if string[i].isdigit() == True:
-        if i != longest_len-1:
-          if string[i+1].isdigit() == True:
-            return 2
-          else:
-            return 1
 
- 
- 
+        self.add_dash(self.string_e,longest_len)
+        self.add_dash(self.string_B,longest_len)
+        self.add_dash(self.string_G,longest_len)
+        self.add_dash(self.string_D,longest_len)
+        self.add_dash(self.string_A,longest_len)
+        self.add_dash(self.string_E,longest_len)
+          
+        return (self.string_e,self.string_B,self.string_G,self.string_D,self.string_A,self.string_E,longest_len)
+        
+     
+     
+     
+     
+     
+    def twelve_note(self,i):
+        """
+        Helper function for vertical_slice. It appends two vertical slices of our tab when we have double digit notes
+        """
+        self.notes.append(self.string_e[i])
+        self.notes.append(self.string_B[i])
+        self.notes.append(self.string_G[i])
+        self.notes.append(self.string_D[i])
+        self.notes.append(self.string_A[i])
+        self.notes.append(self.string_E[i])
+        self.notes.append(self.string_e[i+1])
+        self.notes.append(self.string_B[i+1])
+        self.notes.append(self.string_G[i+1])
+        self.notes.append(self.string_D[i+1])
+        self.notes.append(self.string_A[i+1])
+        self.notes.append(self.string_E[i+1])
+        self.final_notes.append(self.notes)
 
-def vertical_slice(string_e,string_B,string_G,string_D,string_A,string_E,longest_len):
-    """
-    This cell basically takes a vertical slice of the tablature to get a single note/chord and it appends that note/chord into a list
-    Only reason I have more 'repetitive' code is because I need the flag to check for double digit notes,
-    and that uses the continue function
-    """
-    global final_notes,notes
-    final_notes = []
-    flag=True
-    for i in range(longest_len):
-        if flag == False:
-            flag = True
-            continue
-        notes = []
-        if note_check(string_e,i,longest_len) == 2:
-            twelve_note(i)
-            flag = False
-            continue
-        elif note_check(string_e,i,longest_len) == 1:
-            six_note(i)
-            continue
-        if note_check(string_B,i,longest_len) == 2:
-            twelve_note(i)
-            flag = False
-            continue
-        elif note_check(string_B,i,longest_len) == 1:
-            six_note(i)
-            continue
-        if note_check(string_G,i,longest_len) == 2:
-            twelve_note(i)
-            flag = False
-            continue
-        elif note_check(string_G,i,longest_len) == 1:
-            six_note(i)
-            continue
-        if note_check(string_D,i,longest_len) == 2:
-            twelve_note(i)
-            flag = False
-            continue
-        elif note_check(string_D,i,longest_len) == 1:
-            six_note(i)
-            continue
-        if note_check(string_A,i,longest_len) == 2:
-            twelve_note(i)
-            flag = False
-            continue
-        elif note_check(string_A,i,longest_len) == 1:
-            six_note(i)
-            continue
-        if note_check(string_E,i,longest_len) == 2:
-            twelve_note(i)
-            flag = False
-            continue
-        elif note_check(string_E,i,longest_len) == 1:
-            six_note(i)
-            continue
-    return final_notes
- 
- 
-def remove_wrong_notes(final_notes):
-    """
-    Function that processes final notes to get rid of wrongly processed notes. For example,
-    consecutive 5's can be read as 55, but the guitar only has 24 frets.
-    """
-    
-    #Array to store indices of notes which are to be removed
-    removal_index = []
-
-    for note in range(len(final_notes)):
-      if len(final_notes[note]) > 8:
-        for i in range(6):
-          if final_notes[note][i].isdigit() == True and final_notes[note][i+6].isdigit() == True:
-            if (int(final_notes[note][i] + final_notes[note][i+6])) > 24:
-              final_notes.append(final_notes[note][0:6])
-              final_notes.append(final_notes[note][6:12])
-              removal_index.append(note)
-              break
+    def six_note(self,i):
+        """
+        Helper function for vertical_slice. It appends one vertical slice of our tab
+        """
+        self.notes.append(self.string_e[i])
+        self.notes.append(self.string_B[i])
+        self.notes.append(self.string_G[i])
+        self.notes.append(self.string_D[i])
+        self.notes.append(self.string_A[i])
+        self.notes.append(self.string_E[i])
+        self.final_notes.append(self.notes)
 
 
-    #Get rid of the wrong notes
-    final_notes = [i for j, i in enumerate(final_notes) if j not in removal_index]
+    def note_check(self,string,i,longest_len):
+        """
+        Helper function for vertical_slice. It checks whether we have a 2 digit or 1 digit note
+        """
+        if string[i].isdigit() == True:
+            if i != longest_len-1:
+              if string[i+1].isdigit() == True:
+                return 2
+              else:
+                return 1
 
-    #Join different string notes or dashes
-    for i in range(len(final_notes)):
-      final_notes[i] = ''.join(final_notes[i])
-      
-    return final_notes
+     
+     
+
+    def vertical_slice(self,longest_len):
+        """
+        This cell basically takes a vertical slice of the tablature to get a single note/chord and it appends that note/chord into a list
+        Only reason I have more 'repetitive' code is because I need the flag to check for double digit notes,
+        and that uses the continue function
+        """
+
+        flag=True
+        for i in range(longest_len):
+            if flag == False:
+                flag = True
+                continue
+            self.notes = []
+            if self.note_check(self.string_e,i,longest_len) == 2:
+                self.twelve_note(i)
+                flag = False
+                continue
+            elif self.note_check(self.string_e,i,longest_len) == 1:
+                self.six_note(i)
+                continue
+            if self.note_check(self.string_B,i,longest_len) == 2:
+                self.twelve_note(i)
+                flag = False
+                continue
+            elif self.note_check(self.string_B,i,longest_len) == 1:
+                self.six_note(i)
+                continue
+            if self.note_check(self.string_G,i,longest_len) == 2:
+                self.twelve_note(i)
+                flag = False
+                continue
+            elif self.note_check(self.string_G,i,longest_len) == 1:
+                self.six_note(i)
+                continue
+            if self.note_check(self.string_D,i,longest_len) == 2:
+                self.twelve_note(i)
+                flag = False
+                continue
+            elif self.note_check(self.string_D,i,longest_len) == 1:
+                self.six_note(i)
+                continue
+            if self.note_check(self.string_A,i,longest_len) == 2:
+                self.twelve_note(i)
+                flag = False
+                continue
+            elif self.note_check(self.string_A,i,longest_len) == 1:
+                self.six_note(i)
+                continue
+            if self.note_check(self.string_E,i,longest_len) == 2:
+                self.twelve_note(i)
+                flag = False
+                continue
+            elif self.note_check(self.string_E,i,longest_len) == 1:
+                self.six_note(i)
+                continue
+        return self.final_notes
+     
+     
+    def remove_wrong_notes(self,final_notes):
+        """
+        Function that processes final notes to get rid of wrongly processed notes. For example,
+        consecutive 5's can be read as 55, but the guitar only has 24 frets.
+        """
+        
+        #Array to store indices of notes which are to be removed
+        removal_index = []
+
+        for note in range(len(self.final_notes)):
+          if len(self.final_notes[note]) > 8:
+            for i in range(6):
+              if self.final_notes[note][i].isdigit() == True and self.final_notes[note][i+6].isdigit() == True:
+                if (int(self.final_notes[note][i] + self.final_notes[note][i+6])) > 24:
+                  self.final_notes.append(self.final_notes[note][0:6])
+                  self.final_notes.append(self.final_notes[note][6:12])
+                  removal_index.append(note)
+                  break
+
+
+        #Get rid of the wrong notes
+        self.final_notes = [i for j, i in enumerate(self.final_notes) if j not in removal_index]
+
+        #Join different string notes or dashes
+        for i in range(len(self.final_notes)):
+          self.final_notes[i] = ''.join(self.final_notes[i])
+          
+        return self.final_notes
     
     
